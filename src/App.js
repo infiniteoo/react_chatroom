6f473d950b8ef04d987947ee13b01ad31f67d8e6
Troy Dorman <troydorman@gmail.com>
2021-04-26 15:07:35 -0600

    set up react app. installed dependencies. implemented supplied CSS for project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+
+import './App.css'
+
+const App = () => {
+    return (
+        <div>
+            <h2>we're online bitch</h2>
+        </div>
+    )
+}
+
+export default App
