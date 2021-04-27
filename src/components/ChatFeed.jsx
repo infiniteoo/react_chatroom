4c239ee1a5307552b365f45e92ed62069e715c73
Troy Dorman <troydorman@gmail.com>
2021-04-27 10:52:35 -0600

    implemented the read receipt logic to show when other users have read your messages

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -11,6 +11,19 @@ const ChatFeed = (props) => {
 
     console.log("chat, username, messages", chat, userName, messages)
 
+    const renderReadReceipts = (message, isMyMessage) => {
+       return chat.people.map((person, index) => person.last_read === message.id && (
+            <div
+                key={`read_${index}`}
+                className="read-receipt"
+                style={{
+                    float: isMyMessage ? 'right' : 'left',
+                    backgroundImage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ) )
+    }
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
@@ -31,7 +44,7 @@ const ChatFeed = (props) => {
                         }
                     </div>
                     <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-                        read-receipts 
+                        {renderReadReceipts(message, isMyMessage)} 
                     </div>
                 </div>
             )
